78d8ce0671d7bea9eba3e88f35d359a71b0c5d06
JamaHCS <jama117@hotmail.com>
2020-04-12 18:39:15 -0500

    5.- Estadio POO

--- src/Vineyard.java
+++ src/Vineyard.java
@@ -40,55 +40,13 @@ d)	Verificar si hubo algún año en el cual no se produjo el vino tipo 3. Si exi
 
         System.out.println(Tools.ANSI_GREEN + "La producción total de todos los años, fue de : " + totalProduction + "lt" + Tools.ANSI_RESET);
         if (yearless != -1) {
-            System.out.println("En el año " + yearless + " se produjeron 0 lt de vino del tipo 3");
+            System.out.println("En el año " + (yearless + 1) + " se produjeron 0 lt de vino del tipo 3");
         }
-        System.out.println("En el año " + maxyer + " se producjeron " + ltOfMasYear + " lt y fue el año que mas producción del tipo 2 hubo.");
+        System.out.println("En el año " + (maxyer + 1) + " se producjeron " + ltOfMasYear + " lt y fue el año que mas producción del tipo 2 hubo.");
 
     }
 
 
 //    Do you think that if life fulfilled wishes, it would live?
 
-    /*
-    public static void show() {
-        int aux = 1;
-        int aux2 = 0;
-
-        int maxYear = 0;
-        int lessType3 = 0;
-
-        Scanner theScanner = new Scanner(System.in);
-        System.out.println("¿Por cuantos años se añejará?");
-        int time = theScanner.nextInt();
-
-        for (int i = 0; i < time; i++) {
-            System.out.println("Ingrese los litros de vino del tipo 1");
-            int type1 = theScanner.nextInt();
-            System.out.println("Ingrese los litros de vino del tipo 2");
-            int type2 = theScanner.nextInt();
-            System.out.println("Ingrese los litros de vino del tipo 3");
-            int type3 = theScanner.nextInt();
-            System.out.println("Ingrese los litros de vino del tipo 4");
-            int type4 = theScanner.nextInt();
-            System.out.println("Ingrese los litros de vino del tipo 5");
-            int type5 = theScanner.nextInt();
-
-            int add = type1 + type2 + type3 + type4 + type5;
-            System.out.println("La suma del año " + aux + " es:" + add);
-
-            if (type2 > aux2) {
-                aux2 = type2;
-                maxYear = i;
-            }
-
-            if (type3 == 0) {
-                lessType3 = aux - 1;
-            }
-        }
-
-        System.out.println("Año en que se produjo la mayor cantidad de litros tipo 2: " + maxYear);
-        System.out.println("Total de litros tipo dos en ese año: " + aux2);
-        System.out.println("Año en el que no se produjo vino del tipo tres: " + lessType3);
-    }
-     */
 }
